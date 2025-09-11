9935c2df79afab33f50b804010ec8972e6b3b79a
Rashika <rashikaaifariyanto2000@gmail.com>
2025-09-11 20:20:03 +0700

    feat: setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
- /** @type {import('tailwindcss').Config} */
-export default {
-   content: ["./src/**/*.{html,js}"],
-   theme: {
-     extend: {},
-   },
-   plugins: [],
- }
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
